b5442af895cbb97f8b9efb4e1af6a496826222ee
Maggie <mzhao3@stuy.edu>
2018-12-19 22:17:12 -0500

    working

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,6 +1,6 @@
 #include "pipe_networking.h"
 
-char *WKP = "WKPipe";
+char *WKP = "server";
 /*=========================
   server_handshake
   args: int * to_client
@@ -10,41 +10,50 @@ char *WKP = "WKPipe";
 
   returns the file descriptor for the upstream pipe.
   =========================*/
-
 int server_handshake(int *to_client) {
+  //reads first
   remove(WKP);
   char buf[256];
-  int read_end, write_end;
-  if (mkfifo(WKP, 0666) == 1)
-    perror("Server pipe failure.");
+  int fd;
 
-  printf("Opening up the pipe...\n");
-  read_end = open( WKP, O_RDONLY );
-  if (read_end < 0)
+  //creates the WKP
+  if (mkfifo(WKP, 0666) < 0) {
+    perror("Server pipe failure.");
+    exit(0);
+  }
+
+  printf("Well known pipe created.\n");
+
+  //open WKP for reading
+  printf("Waiting for client.\n");
+  fd = open( WKP, O_RDONLY );
+  if (fd < 0)
     perror("Cannot open pipe.");
 
   printf("Reading the pipe...\n");
-  read(read_end, buf, 256);
+  read(fd, buf, sizeof(buf));
   printf("This pipe says %s\n\n", buf);
 
   printf("Write back bro.\n");
-  write_end = open(buf, O_WRONLY);
-  if (write_end < 0)
+  // opens in write mode
+  *to_client = open(buf, O_WRONLY);
+  if (*to_client < 0)
     perror("Writing side malfunction...\n");
 
   printf("Our brains connected. Sending brainwaves. Be on the lookout.\n");
-  if ( write(write_end, "shwooooom", strlen("shwooooom")) < 0)
+  int w = write(*to_client, "shwooooom", strlen("shwooooom"));
+  if ( w < 0)
     perror ("your writing bad");
 
-  read(read_end, buf, 256);
+  printf("Waiting for client...\n");
+  read(fd, buf, sizeof(buf));
   if (strlen(buf) < 1) {
     printf("uhhhhh\n");
     return -1;
   }
   printf("We done it! %s recieved.\n", buf);
 
-  *to_client = write_end;
-  return read_end;
+  return fd;
 }
 
 
@@ -58,36 +67,35 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  remove (WKP);
-  int read_end, write_end;
+  int fd;
   char buf[256];
-  char *pipe = "client";
+  char *pipe = "private";
 
-  if (mkfifo(pipe, 0666) == -1)
+  if (mkfifo(pipe, 0666) < 0) {
     perror("Client pipe failure.");
+    exit(0);
+  }
+  printf("Private pipe created.\n");
 
-  printf("Pipe created.\n");
+  printf("Writing to server about private pipe.\n");
+  *to_server = open(WKP, O_WRONLY);
+  write(*to_server, pipe, sizeof(pipe));
 
-  printf("Write something\n");
-  write_end = open(pipe, O_WRONLY);
-  if (write_end == -1)
-    perror("Why can't you write correctly -_-.\n");
+  fd = open(pipe, O_RDONLY);
+  printf("Waiting for server. \n");
 
-  write(write_end, pipe, strlen(pipe)+1);
-
-  read_end = open(pipe, O_RDONLY);
-  if (read_end == -1)
+  if (fd == -1){
     perror("Cannot open pipe.");
-  printf("waiting for the pipe.\n");
-  read(read_end, buf, 256);
+    exit(0);
+  }
+  read(fd, buf, sizeof(buf));
   printf("Your message, your highness: %s\n", buf);
 
-  printf("Now write back!\n");
-  write(write_end, "your mom!", strlen("your mom!"));
   remove(pipe);
 
-  *to_server = write_end;
-  return read_end;
+  printf("Now write back!\n");
+  write(*to_server, "your mom!", strlen("your mom!"));
 
-  return 0;
+  return fd;
+
 }
